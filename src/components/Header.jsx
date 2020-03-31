b2e44cb4fb48d4da1d54ee2aa7c18c20915b28d2
aman-atg <amanansari.a2@gmail.com>
2020-03-31 17:17:02 +0530

    basic markup

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="content">
+        <h2>
+          <span className="new">New</span> Monograph Dashboard
+        </h2>
+        <h1>Powerful Insight into your team</h1>
+        <p>Project planning and time tracking for agile teams</p>
+        <div className="header_footer">
+          <button>Schedule a Demo </button> <span>to see a preview</span>
+        </div>
+      </div>
+      <div className="image">
+        <img
+          src="/images/illustration-devices.svg"
+          type="Illustration Devices"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
